e483c513db31873706c2d2b51df24a355bf4d3d7
ino <syookiti@gmail.com>
2025-06-09 01:01:09 +0900

    ASが一応動いた

--- ros_src/my_mobile_robot/src/ASSubCrawlerDriveController.cpp
+++ ros_src/my_mobile_robot/src/ASSubCrawlerDriveController.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 #include <mutex>
 
-class SubCrawlerDriveController : public cnoid::SimpleController
+class ASSubCrawlerDriveController : public cnoid::SimpleController
 {
     public:
         virtual bool configure(cnoid::SimpleControllerConfig* config) override;
@@ -13,7 +13,7 @@ class SubCrawlerDriveController : public cnoid::SimpleController
         virtual bool control() override;
         virtual void unconfigure() override;
     private:
-        cnoid::Link* joints[2];
+        cnoid::Link* joints[4];
         rclcpp::Node::SharedPtr node;
         rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr subscription;
         geometry_msgs::msg::Vector3 command;
@@ -23,9 +23,9 @@ class SubCrawlerDriveController : public cnoid::SimpleController
         double dt;
 };
 
-CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(SubCrawlerDriveController)
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(ASSubCrawlerDriveController)
 
-bool SubCrawlerDriveController::configure(cnoid::SimpleControllerConfig* config)
+bool ASSubCrawlerDriveController::configure(cnoid::SimpleControllerConfig* config)
 {
     node = std::make_shared<rclcpp::Node>(config->controllerName());
     subscription = node->create_subscription<geometry_msgs::msg::Vector3>(
@@ -40,13 +40,15 @@ bool SubCrawlerDriveController::configure(cnoid::SimpleControllerConfig* config)
     return true;
 }
 
-bool SubCrawlerDriveController::initialize(cnoid::SimpleControllerIO* io)
+bool ASSubCrawlerDriveController::initialize(cnoid::SimpleControllerIO* io)
 {
     auto body = io->body();
     dt = io->timeStep();
-    joints[0] = body->joint("SubCraw_L_joint");
-    joints[1] = body->joint("SubCraw_R_joint");
-    for(int i=0; i < 2; ++i){
+    joints[0] = body->joint("FL_FLIPPER_JOINT");
+    joints[1] = body->joint("FR_FLIPPER_JOINT");
+    joints[2] = body->joint("BL_FLIPPER_JOINT");
+    joints[3] = body->joint("BR_FLIPPER_JOINT");
+    for(int i=0; i < 4; ++i){
         auto joint = joints[i];
         joint->setActuationMode(JointDisplacement);
         io->enableIO(joint);
@@ -54,27 +56,40 @@ bool SubCrawlerDriveController::initialize(cnoid::SimpleControllerIO* io)
         // io->enableInput(joint, JointTorque);
         // io->enableOutput(joint, JointTorque);
     }
+    // for(int i=0; i < 2; ++i){
+    //     auto joint = joints[i+2];
+    //     joint->setActuationMode(JointTorque);
+    //     io->enableInput(joint, JointTorque);
+    //     io->enableOutput(joint, JointTorque);
+    // }
     return true;
 }
-bool SubCrawlerDriveController::control()
+bool ASSubCrawlerDriveController::control()
 {
-    constexpr double kd = 1;
-    double q_target[2];
+    constexpr double kd = 1.5;
+    double q_target[4];
 
 {
         std::lock_guard<std::mutex> lock(commandMutex);
         q_target[0] = command.z;
         q_target[1] = command.y;
+        q_target[2] = command.x;
+        q_target[3] = command.x;
 }
 
-    for(int i=0; i < 2; ++i){
+    for(int i=0; i < 4; ++i){
         auto joint = joints[i];
         joint->q_target() = joint->q() + kd * q_target[i] * dt;
     }
+    // for(int i=0; i < 2; ++i){
+    //     auto joint = joints[i+2];
+    //     joint->q_target() = joint->q();
+    //     // joint->u() = 0;
+    // }
     return true;
 }
 
-void SubCrawlerDriveController::unconfigure()
+void ASSubCrawlerDriveController::unconfigure()
 {
     if(executor){
         executor->cancel();
---
+++ ros_src/my_mobile_robot/src/AizuSpiderDriveController.cpp
@@ -0,0 +1,84 @@
+#include <cnoid/SimpleController>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <memory>
+#include <thread>
+#include <mutex>
+
+class AizuSpiderDriveController : public cnoid::SimpleController
+{
+    public:
+        virtual bool configure(cnoid::SimpleControllerConfig* config) override;
+        virtual bool initialize(cnoid::SimpleControllerIO* io) override;
+        virtual bool control() override;
+        virtual void unconfigure() override;    private:
+    private:
+        cnoid::Link* wheels[2];
+        rclcpp::Node::SharedPtr node;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription;
+        geometry_msgs::msg::Twist command;
+        rclcpp::executors::StaticSingleThreadedExecutor::UniquePtr executor;
+        std::thread executorThread;
+        std::mutex commandMutex;};
+
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(AizuSpiderDriveController)
+
+bool AizuSpiderDriveController::configure(cnoid::SimpleControllerConfig* config)
+{
+    node = std::make_shared<rclcpp::Node>(config->controllerName());
+    subscription = node->create_subscription<geometry_msgs::msg::Twist>(
+    "/cmd_vel", 1,
+    [this](const geometry_msgs::msg::Twist::SharedPtr msg){
+        std::lock_guard<std::mutex> lock(commandMutex);
+        command = *msg;
+    });
+    executor = std::make_unique<rclcpp::executors::StaticSingleThreadedExecutor>();
+    executor->add_node(node);
+    executorThread = std::thread([this](){executor->spin();});
+    return true;
+}
+
+
+bool AizuSpiderDriveController::initialize(cnoid::SimpleControllerIO* io)
+{
+    auto body = io->body();
+    wheels[0] = body->link("L_TRACK");
+    wheels[1] = body->link("R_TRACK");
+    for(int i=0; i < 2; ++i){
+        auto wheel = wheels[i];
+        wheel->setActuationMode(JointVelocity);
+        io->enableOutput(wheel, JointVelocity);
+    }
+    return true;
+}
+
+bool AizuSpiderDriveController::control()
+{
+    constexpr double wheelRadius = 0.176;
+    constexpr double halfAxleWidth = 0.3;
+    constexpr double kd = 0.3;
+    double dq_target[2];
+
+{
+        std::lock_guard<std::mutex> lock(commandMutex);
+        double dq_x = command.linear.x / wheelRadius;
+        double dq_yaw = command.angular.z * halfAxleWidth / wheelRadius;
+        dq_target[0] = dq_x - dq_yaw;
+        dq_target[1] = dq_x + dq_yaw;
+}
+
+    for(int i=0; i < 2; ++i){
+        wheels[i]->dq_target() = kd * (dq_target[i] - wheels[i]->dq_target()); //速度のP制御
+    }
+    return true;
+}
+
+void AizuSpiderDriveController::unconfigure()
+{
+    if(executor){
+        executor->cancel();
+        executorThread.join();
+        executor->remove_node(node);
+        executor.reset();
+    }
+}
